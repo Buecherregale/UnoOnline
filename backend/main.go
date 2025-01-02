2e36712190d0f5ecec6da16db247ddc1f644d5f9
= <david.grimmen@gmail.com>
2025-01-02 21:00:09 +0100

    rooms api

--- backend/main.go
+++ backend/main.go
@@ -1,7 +1,22 @@
 package main
 
-import "fmt"
+import (
+	"net/http"
+	"uno_online/controller"
+)
 
 func main() {
-	fmt.Println("Hello, World!")
+	mux := Router()
+	http.ListenAndServe(":8080", mux)
+}
+
+func Router() *http.ServeMux {
+	mux := http.NewServeMux()
+	mux.HandleFunc("POST /player", controller.CreatePlayer)
+	mux.HandleFunc("POST /room", controller.CreateRoom)
+	mux.HandleFunc("POST /room/{rId}", controller.Start)
+	mux.HandleFunc("POST /room/{rId}/players", controller.JoinRoom)
+	mux.HandleFunc("DELETE /room/{rId}/players", controller.LeaveRoom)
+
+	return mux
 }
