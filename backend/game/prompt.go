912da857884fbec174991b70b0039b4a9059db01
= <david.grimmen@gmail.com>
2025-01-18 22:59:06 +0100

    added websocket communication

--- backend/game/prompt.go
+++
@@ -1,5 +0,0 @@
-package game
-
-func PromptChoice[T any](target *GamePlayer, choices []T) T {
-	return choices[0]
-}
--- backend/uno/special_cards.go
+++ backend/uno/special_cards.go
@@ -1,40 +1,100 @@
 package uno
 
-import "uno_online/game"
+import (
+	"math/rand"
 
-func reversePlayed(gp *game.GamePlayer, card game.Card, state *game.GameState) {
+	"uno_online/api/ws"
+	"uno_online/game"
+)
+
+func reversePlayed(gp *game.GamePlayer, card *game.Card, state *game.GameState) {
+	u, _ := (*card).(UnoCard)
+	if u.Value != Reverse {
+		return
+	}
 	state.CurrDir *= -1
+	state.WsRoom.BroadcastMessage("DirectionChangedPayload", ws.DirectionChangedPayload{
+		Direction: state.CurrDir,
+	})
 }
 
-func skipPlayed(gp *game.GamePlayer, card game.Card, state *game.GameState) {
+func skipPlayed(gp *game.GamePlayer, card *game.Card, state *game.GameState) {
+	u, _ := (*card).(UnoCard)
+	if u.Value != Skip {
+		return
+	}
+	state.WsRoom.BroadcastMessage("PlayerSkippedPayload", ws.PlayerSkippedPayload{})
 	state.NextPlayer()
 }
 
-func plusTwoPlayed(gp *game.GamePlayer, card game.Card, state *game.GameState) {
+func plusTwoPlayed(gp *game.GamePlayer, card *game.Card, state *game.GameState) {
+	u, _ := (*card).(UnoCard)
+	if u.Value != Plus2 {
+		return
+	}
+
 	p := state.PeekNextPlayer()
 	state.DrawCards(p, 2)
 	state.NextPlayer()
+	state.WsRoom.BroadcastMessage("PlayerSkippedPayload", ws.PlayerSkippedPayload{})
 }
 
-func wildcardPlayed(gp *game.GamePlayer, card game.Card, state *game.GameState) {
-	choice := game.PromptChoice(gp, []Color{Red, Green, Blue, Yellow})
-	u, _ := card.(UnoCard)
+func wildcardPlayed(gp *game.GamePlayer, card *game.Card, state *game.GameState) {
+	u, _ := (*card).(UnoCard)
+	if u.Color != Black || u.Value != Wildcard {
+		return
+	}
+
+	choiceInt, timeout := game.AskColor(gp, colorsToInts(basicColors))
+	choice := Color(choiceInt)
+	if timeout {
+		choice = basicColors[rand.Intn(len(basicColors))]
+	}
 	u.Chosen = choice
 
+	state.WsRoom.BroadcastMessage("PlayerChoseColorPayload", ws.PlayerChoseColorPayload{
+		PlayerId: gp.P.Id,
+		Name:     gp.P.Name,
+		Color:    choiceInt,
+	})
 	state.NextPlayer()
+	state.WsRoom.BroadcastMessage("PlayerSkippedPayload", ws.PlayerSkippedPayload{})
 }
 
-func wildcard4Played(gp *game.GamePlayer, card game.Card, state *game.GameState) {
-	choice := game.PromptChoice(gp, []Color{Red, Green, Blue, Yellow})
-	u, _ := card.(UnoCard)
+func wildcard4Played(gp *game.GamePlayer, card *game.Card, state *game.GameState) {
+	u, _ := (*card).(UnoCard)
+	if u.Color != Black || u.Value != Wildcard4 {
+		return
+	}
+
+	choiceInt, timeout := game.AskColor(gp, colorsToInts(basicColors))
+	choice := Color(choiceInt)
+	if timeout {
+		choice = basicColors[rand.Intn(len(basicColors))]
+	}
 	u.Chosen = choice
+
+	state.WsRoom.BroadcastMessage("PlayerChoseColorPayload", ws.PlayerChoseColorPayload{
+		PlayerId: gp.P.Id,
+		Name:     gp.P.Name,
+		Color:    choiceInt,
+	})
 
 	p := state.PeekNextPlayer()
 	state.DrawCards(p, 4)
 
 	state.NextPlayer()
+	state.WsRoom.BroadcastMessage("PlayerSkippedPayload", ws.PlayerSkippedPayload{})
 }
 
 func UnoCardPlacedListeners() []game.CardPlayEventListener {
 	return []game.CardPlayEventListener{reversePlayed, skipPlayed, plusTwoPlayed, wildcardPlayed, wildcard4Played}
+}
+
+func colorsToInts(colors []Color) []int {
+	asInt := make([]int, len(colors))
+	for i, c := range colors {
+		asInt[i] = int(c)
+	}
+	return asInt
 }
