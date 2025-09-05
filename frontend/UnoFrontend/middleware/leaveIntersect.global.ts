631d516d24c169636d2041360706564e4921a34d
Julius <clashzocker2008@gmail.com>
2025-09-06 00:32:30 +0200

    fix: go into game without getting popup for leaving

--- frontend/UnoFrontend/middleware/leaveIntersect.global.ts
+++ frontend/UnoFrontend/middleware/leaveIntersect.global.ts
@@ -9,7 +9,7 @@ export default defineNuxtRouteMiddleware((to, from) => {
   if (import.meta.server) return;
 
   // Check if leaving a lobby and navigating to different page
-  if (from.fullPath.indexOf("/lobby") > -1 && to.fullPath !== from.fullPath) {
+  if (from.fullPath.indexOf("/lobby") > -1 && to.fullPath !== from.fullPath && !to.fullPath.includes("/game")) {
     // Show confirmation dialog before leaving lobby
     if (!window.confirm("You are in a Lobby are you Sure you want to leave?")) {
       return abortNavigation();
