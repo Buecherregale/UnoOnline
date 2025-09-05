8a4030c55945a5a079514a4b59774ce61920932b
buecherregale <david.grimmmen@gmail.com>
2025-09-05 23:42:06 +0200

    remove old files

--- backend/models/room.go
+++
@@ -1,9 +0,0 @@
-package models
-
-import "github.com/google/uuid"
-
-type Room struct {
-  Id        uuid.UUID   `json:"id"`
-  Players   []uuid.UUID `json:"players"`
-  Owner     []uuid.UUID `json:"owner"`
-}
