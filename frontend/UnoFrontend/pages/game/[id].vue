55e0842ffbc694150bc2f45762466721cb1800c1
Julius <clashzocker2008@gmail.com>
2025-09-06 17:22:29 +0200

    chore: prettier

--- frontend/UnoFrontend/pages/game/[id].vue
+++ frontend/UnoFrontend/pages/game/[id].vue
@@ -1,6 +1,6 @@
 <script setup lang="ts">
-import type {Player, Room} from "~/util/models";
-import {getIDFromCookie} from "~/util/getIDFromCookie";
+import type { Player, Room } from "~/util/models";
+import { getIDFromCookie } from "~/util/getIDFromCookie";
 
 const route = useRoute();
 const gameId = route.params.id;
