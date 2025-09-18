e062a34b3605b3a503831b363279d26c6850fb72
Julius <clashzocker2008@gmail.com>
2025-09-18 12:09:24 +0200

    feat: enhance WebSocket handling with singleton instance and room update callback

--- frontend/UnoFrontend/util/webSocketHelper.ts
+++ frontend/UnoFrontend/util/webSocketHelper.ts
@@ -1,9 +1,10 @@
-import type {message, Player} from "~/util/models";
+import type {message, Player, Room} from "~/util/models";
 
 export default class WebSocketHelper {
     private socket: WebSocket | null = null;
     private messageQueue: message[] = [];
     private isConnected: boolean = false;
+    private onRoomUpdateCallback: ((room: Room) => void) | null = null;
 
     constructor(playerId: string, roomId: string) {
         let url = `ws://localhost:8080/ws?roomId=${roomId}&playerId=${playerId}`;
@@ -18,6 +19,15 @@ export default class WebSocketHelper {
         this.socket.onmessage = (event) => {
             // Nachrichten vom Server verarbeiten
             console.log('Message received:', event.data);
+            // check payload type
+            const msg: message = JSON.parse(event.data);
+            if (msg.type === 'RoomJoinPayload') {
+                // update room
+                const roomData: Room = JSON.parse(msg.payload);
+                if (this.onRoomUpdateCallback) {
+                    this.onRoomUpdateCallback(roomData);
+                }
+            }
         };
 
         this.socket.onclose = () => {
@@ -59,8 +69,12 @@ export default class WebSocketHelper {
     }
 
     public playerJoined(player: Player): void {
-        const message: message = { type: 'RoomJoin', payload: JSON.stringify(player) };
+        const message: message = { type: 'RoomJoinPayload', payload: JSON.stringify(player) };
         this.queueMessage(message);
+    }
+
+    public setRoomUpdateCallback(callback: (room: Room) => void): void {
+        this.onRoomUpdateCallback = callback;
     }
 
     public disconnect(): void {
