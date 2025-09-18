b42a6f5a32c5296719df1e054f042116e134cf3b
Julius <clashzocker2008@gmail.com>
2025-09-18 23:44:16 +0200

    feat: add clipboard functionality to copy room ID

--- frontend/UnoFrontend/nuxt.config.ts
+++ frontend/UnoFrontend/nuxt.config.ts
@@ -4,7 +4,7 @@ import {process} from "std-env";
 export default defineNuxtConfig({
   compatibilityDate: '2024-11-01',
   devtools: { enabled: true },
-  modules: ['@nuxtjs/tailwindcss'],
+  modules: ['@nuxtjs/tailwindcss', '@vueuse/nuxt'],
   runtimeConfig: {
     public: {
       apiBase: process.env.API_BASE_URL || 'http://backend:8080'
